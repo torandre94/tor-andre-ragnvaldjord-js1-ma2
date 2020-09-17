7aaef503581cceb4f9794a2fca1aa24c28d7cee0
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-09-17 11:57:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ button.onclick = function() {
 }
 
 // question 2
+const buttonExtra = document.querySelector(".toggler");
+
+buttonExtra.onclick = function() {
+    document.querySelector(".togglee").classList.toggle("extra");
+}
+
+// question 3
+const dropdown = document.querySelector("select");
+const paragraphsContainer = document.querySelector(".paragraphs-container");
+
+
+dropdown.onchange = function() {
+    const selectedValue = this.value;
+
+    paragraphsContainer.innerHTML = "";
+
+    for (let i = 1; i <= selectedValue; i++) {
+        paragraphsContainer.innerHTML += '<p class="content"></p>';
+    }
+};
+
+//question 4
+
+
+
 
